5683484379d805ef093e25beae61814b4ed4a543
ojasva2011 <90315077+ojasva2011@users.noreply.github.com>
2022-05-28 14:40:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+ function preload(){
+     classifier=ml5.imageClassifier("DoodleNet");
+ }
+
+ function setup(){
+     canvas=createCanvas( 280,280);
+     canvas.center();
+     background("white");
+     canvas.mouseReleased(classifyCanvas);
+     synth = window.speechSynthesis;
+ }
+
+ function draw(){
+   strokeWeight(14);
+   stroke(0);
+
+   if(mouseIsPressed){
+      line(pmouseX,pmouseY,mouseX,mouseY);
+   }
+ }
+
+ function clearCanvas(){
+     background("white")
+ }
+
+ function classifyCanvas(){
+     classifier.classify(canvas,gotresult);
+ }
+
+ function gotresult(error,results){
+    if(error){
+        console.error(error);
+    }
+   
+        console.log(results);
+        document.getElementById("label").innerHTML="label :"+results[0].label;
+        document.getElementById("confidence").innerHTML="confidence :"+ Math.round(results[0].confidence * 100)+" %";
+        utterThis=new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterThis);
+ 
+
+ }
